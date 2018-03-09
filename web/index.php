0a944d7d4ca7a733184127270b7d39fdf0fd1490
EvgenVysotski <36482132+EvgenVysotski@users.noreply.github.com>
2018-03-10 00:19:25 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
 	<meta charset="UTF-8">
 	<title>alpine a110</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro|Titillium+Web" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script defer src="js/fontawesome-all.js"></script>
@@ -103,13 +104,13 @@
                          </svg>
                     </a>
                     <a href="https://vk.com" target="_blank" title="вконтакте">
-                	    <svg class="cocial" width="49" height="49" xmlns="http://www.w3.org/2000/svg"><defs><filter height="200%" width="200%" y="-50%" x="-50%" id="svg_1_blur"><feGaussianBlur stdDeviation="0" in="SourceGraphic"/>  </filter> </defs> <g><rect fill="#45668e"  height="51" width="51" y="-1" x="-1"/><rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/></g> <g><text  font-weight="bold" font-style="normal" filter="url(#svg_1_blur)" xml:space="preserve" text-anchor="start" font-family="Arvo, sans-serif" font-size="47" id="svg_1" y="39.59949" x="7.93285" stroke-width="0" stroke="#000" fill="white">В</text> </g></svg>
+                	    <svg class="cocial" width="49" height="49" xmlns="http://www.w3.org/2000/svg"><defs><filter height="200%" width="200%" y="-50%" x="-50%" id="svg_1_blur"><feGaussianBlur stdDeviation="0" in="SourceGraphic"/>  </filter> </defs> <g><rect fill="#45668e"  height="51" width="51" y="-1" x="-1"/><rect fill="url(#gridpattern)" stroke-width="0" y="0" x="0" height="100%" width="100%"/></g> <g><text  font-weight="bold" font-style="normal" filter="url(#svg_1_blur)" xml:space="preserve" text-anchor="start" font-family="Arvo, sans-serif" font-size="47"  y="39.59949" x="7.93285" stroke-width="0" stroke="#000" fill="white">В</text> </g></svg>
                       </a>
                       <a href="https://twitter.com" target="_blank" title="twitter">
-                      	 <svg width="50" height="50" class="cocial" xmlns="http://www.w3.org/2000/svg"> <g><rect fill="#007bb6"  height="52" width="52" y="-1" x="-1"/></g><g><text font-weight="bold" xml:space="preserve" text-anchor="start" font-family="Oswald, sans-serif" font-size="57" id="svg_1" y="44.10941" x="0.41985" stroke-width="0" stroke="#000" fill="white">in</text> </g>
+                      	 <svg width="50" height="50" class="cocial" xmlns="http://www.w3.org/2000/svg"> <g><rect fill="#007bb6"  height="52" width="52" y="-1" x="-1"/></g><g><text font-weight="bold" xml:space="preserve" text-anchor="start" font-family="Oswald, sans-serif" font-size="57" y="44.10941" x="0.41985" stroke-width="0" stroke="#000" fill="white">in</text> </g>
                        </svg>
                       </a>
-                   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2350.2724979862965!2d27.520546415577726!3d53.909133480100785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbc55f35bf3dad%3A0xaabda25bdeeb761f!2z0YPQuy4g0KHQutGA0YvQs9Cw0L3QvtCy0LAgMTQsINCc0LjQvdGB0Lo!5e0!3m2!1sru!2sby!4v1520419392892" width="350" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2350.2724979862965!2d27.520546415577726!3d53.909133480100785!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46dbc55f35bf3dad%3A0xaabda25bdeeb761f!2z0YPQuy4g0KHQutGA0YvQs9Cw0L3QvtCy0LAgMTQsINCc0LjQvdGB0Lo!5e0!3m2!1sru!2sby!4v1520419392892" width="350" height="300" border="0" style="border:0" allowfullscreen></iframe>
                 </div>
                 <div class="down">
                      Copyright 2018. All rights reserved.                    
