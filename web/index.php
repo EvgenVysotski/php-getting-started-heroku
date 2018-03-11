114cfaef22a3995936710a7fe701ce4afd319ad8
EvgenVysotski <36482132+EvgenVysotski@users.noreply.github.com>
2018-03-11 11:06:54 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <script defer src="js/fontawesome-all.js"></script>
+   
    
 </head>
 <body>
@@ -191,6 +191,6 @@
     }   
 
 </script>
-
+ <script defer src="js/fontawesome-all.js"></script>
 </body>
 </html>
